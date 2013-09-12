788baf6962cadf8fb6999c5873dd9926f21e5131
chanthou <chanthou@web-essentials.asia>
2013-09-12 19:04:32 +0700

    Add new file

---
+++ Script/main.js
@@ -0,0 +1,21 @@
+function initialize() {
+	var mapOptions = {
+		center : new google.maps.LatLng(12.712312, 104.888338),
+		zoom : 24,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
+			mapOptions);
+}
+
+
+
+function loadScript() {
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDy4Ne6caAkbNNvOBjGqKXNCzotMhhkRV8&sensor=false&'
+			+ 'callback=initialize';
+	document.body.appendChild(script);
+}
+
+window.onload = loadScript;
